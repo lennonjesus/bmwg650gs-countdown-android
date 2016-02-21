658d3b59da4e1a97fdefc04b82fa51bc1c544931
Lennon Jesus <lennon.jesus@gmail.com>
2016-02-21 05:28:30 -0300

    permitindo compartilhamento da imagem do EN 2016

--- app/src/main/java/net/bmwg650gs/countdown/FullscreenActivity.java
+++ app/src/main/java/net/bmwg650gs/countdown/FullscreenActivity.java
@@ -2,7 +2,9 @@ package net.bmwg650gs.countdown;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.CountDownTimer;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +13,10 @@ import android.text.format.Time;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import static java.lang.String.format;
 
@@ -56,12 +62,7 @@ public class FullscreenActivity extends AppCompatActivity {
             // Note that some of these constants are new as of API 16 (Jelly Bean)
             // and API 19 (KitKat). It is safe to use them, as they are inlined
             // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
         }
     };
     private View mControlsView;
@@ -179,20 +180,28 @@ public class FullscreenActivity extends AppCompatActivity {
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 
-    public void share(View view) {
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Compartilhe com seus amigos!");
+    public void share(View view) throws FileNotFoundException {
+
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("image/png");
+
+        Uri uri = Uri.parse("android.resource://net.bmwg650gs/drawable/" + R.drawable.logo_nacional);
+
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+
         StringBuilder shareMessage = new StringBuilder();
 
         shareMessage
                 .append(format("Faltam %2d dias para o 5º Encontro Nacional bmwg650gs.net", daysLeft))
                 .append("\n")
-                .append("https://goo.gl/NhPpNy")
+                .append("\n")
+                .append("Baixe o app para Android: https://goo.gl/NhPpNy")
         ;
 
         shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage.toString());
-        startActivity(Intent.createChooser(shareIntent, "Compartlhar via..."));
+        startActivity(Intent.createChooser(shareIntent, "Compartilhe..."));
+
     }
 
     public void countdown() {
