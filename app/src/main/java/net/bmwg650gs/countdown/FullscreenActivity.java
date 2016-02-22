a644c3ca80db65ad6e9cce06642b23e47eed3f09
Lennon Jesus <lennon.jesus@gmail.com>
2016-02-22 19:14:06 -0300

    separando em dois botoes o compartilhamento da url do app e o compartilhamento do countdown

--- app/src/main/java/net/bmwg650gs/countdown/FullscreenActivity.java
+++ app/src/main/java/net/bmwg650gs/countdown/FullscreenActivity.java
@@ -33,71 +33,13 @@ public class FullscreenActivity extends AppCompatActivity {
 
     private int daysLeft = 0;
 
-
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
 
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
-        }
-    };
+    private View mContentView;
+
     private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
+
+
 
 
     @Override
@@ -106,82 +48,28 @@ public class FullscreenActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_fullscreen);
 
-        mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
-
-
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
 
         countdown();
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+    public void shareCount(View view) throws FileNotFoundException {
 
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("image/png");
+
+        Uri uri = Uri.parse("android.resource://net.bmwg650gs/drawable/" + R.drawable.logo_nacional);
+
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+
+        shareIntent.putExtra(Intent.EXTRA_TEXT, format("Faltam %2d dias para o 5º Encontro Nacional bmwg650gs.net", daysLeft));
+        startActivity(Intent.createChooser(shareIntent, "Compartilhe a contagem..."));
+
     }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
-
-    public void share(View view) throws FileNotFoundException {
-
+    public void shareApp(View view) {
         Intent shareIntent = new Intent();
         shareIntent.setAction(Intent.ACTION_SEND);
         shareIntent.setType("image/png");
@@ -193,15 +81,13 @@ public class FullscreenActivity extends AppCompatActivity {
         StringBuilder shareMessage = new StringBuilder();
 
         shareMessage
-                .append(format("Faltam %2d dias para o 5º Encontro Nacional bmwg650gs.net", daysLeft))
+                .append("Acompanhe a contagem regressiva para o 5º Encontro Nacional bmwg650gs.net com seu smartphone Android:")
                 .append("\n")
-                .append("\n")
-                .append("Baixe o app para Android: https://goo.gl/NhPpNy")
+                .append("https://goo.gl/NhPpNy")
         ;
 
         shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage.toString());
-        startActivity(Intent.createChooser(shareIntent, "Compartilhe..."));
-
+        startActivity(Intent.createChooser(shareIntent, "Compartilhe o app..."));
     }
 
     public void countdown() {
@@ -245,4 +131,6 @@ public class FullscreenActivity extends AppCompatActivity {
     private int getDaysLeft(long millisUntilFinished) {
         return (int) ((millisUntilFinished / MIL) / SEGUNDOS_POR_DIA);
     }
+
+
 }
